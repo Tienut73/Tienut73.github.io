62607c6eb0037432e7a2682c57074f683bb2680b
Pierce-Logan <38666828+Tienut73@users.noreply.github.com>
2018-06-22 22:23:59 -0500

    Updated Lesson 9

--- assignments/lesson-9/weatherapi.js
+++ assignments/lesson-9/weatherapi.js
@@ -1,16 +1,11 @@
 var weatherRequest = new XMLHttpRequest();
-weatherObject.open('GET','http://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=1346ae4b000772369927f7abc662cda4&units=imperial', 'true= ".png";');
+weatherRequest.open('GET','http://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=1346ae4b000772369927f7abc662cda4&units=imperial', 'true= ".png";');
 
 weatherRequest.send();
 
-weatherRequest.onload = function() {
-    var weatherData = JSON.parse(weatherObject.responseText);
+weatherRequest.onload = function(){
+    var weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
     
-    document.getElementById('currentTemp').innerHTML = weatherData.main.temp;
-    
-    var iconcode = weatherData.weather[0].icon;
-    var icon_path = "http;//openweathermap.org/img/w/" + iconcode + ".png";
-    document.getElementById('weather_icon').src = icon_path;
-    
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
 }
