b822b25b3e7ab2442f3f893ef39649ef0265d544
Pierce-Logan <38666828+Tienut73@users.noreply.github.com>
2018-06-22 21:25:15 -0500

    Updated Lesson 9

--- assignments/lesson-9/weatherapi.js
+++ assignments/lesson-9/weatherapi.js
@@ -1,1 +1,12 @@
 var weatherRequest = new XMLHttpRequest();
+weatherObject.open('GET','http://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=1346ae4b000772369927f7abc662cda4&units=imperial', 'true');
+
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    var weatherData = JSON.parse(weatherObject.responseText);
+    console.log(weatherData);
+    
+    document.getElementById('')
+    
+}
