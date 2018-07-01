ed42d0810c4d45f572f720e8d5e67374ae88cbbe
Pierce-Logan <38666828+Tienut73@users.noreply.github.com>
2018-06-30 23:18:58 -0600

    Final Updates

---
+++ assignments/lesson-10/superheroes/weatherapi.js
@@ -0,0 +1,11 @@
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET','http://api.openweathermap.org/data/2.5/weather?id=4623560&APPID=1346ae4b000772369927f7abc662cda4&units=imperial', 'true= ".png";');
+
+weatherRequest.send();
+
+weatherRequest.onload = function(){
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+}
