ca2fbdc5d7607ae906d41acb2744695da0c6f602
Tienut73 <lucky18975@gmail.com>
2020-05-02 21:17:49 -0500

    Updated CSS

---
+++ lesson2/js/main.js
@@ -0,0 +1,4 @@
+var year = new Date().getFullYear();
+
+document.getElementById("currentYear").innerHTML = "&copy; " + year + " | Logan Pierce | Texas | <a href='https://www.byui.edu/online'>BYUI Online Learning</a><br>";
+document.getElementById("lastUpdated").innerHTML = "Last Updated:" + document.lastModified;
