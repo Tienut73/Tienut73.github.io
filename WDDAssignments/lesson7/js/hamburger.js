e7344500a822050372619d91ed787af9bf63b4ec
Tienut73 <lucky18975@gmail.com>
2020-06-08 13:44:57 -0500

    Updated Index

--- WDDAssignments/lesson7/js/hamburger.js
+++ WDDAssignments/lesson7/js/hamburger.js
@@ -1,3 +1,6 @@
+const hamburger = document.querySelector(".primeNav");
+hamburger.addEventListener("click", toggleMenu, false);
+
 function toggleMenu(){
     console.log("it worked");
  document.getElementById("primeNav").classList.toggle("hide");
